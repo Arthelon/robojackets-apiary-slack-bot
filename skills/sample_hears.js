d9faa337583fdb3140fc30c92ffb235c93e227c6
Daniel Hsing <hsing.daniel@gmail.com>
2018-11-28 21:48:12 -0500

    chore: remove studio from channel_join skill
    Signed-off-by: Daniel Hsing <hsing.daniel@gmail.com>

--- skills/sample_hears.js
+++ skills/sample_hears.js
@@ -9,53 +9,57 @@ respond immediately with a single line response.
 
 */
 
-var wordfilter = require('wordfilter');
+var wordfilter = require("wordfilter");
 
 module.exports = function(controller) {
-
     /* Collect some very simple runtime stats for use in the uptime/debug command */
     var stats = {
         triggers: 0,
-        convos: 0,
-    }
+        convos: 0
+    };
 
-    controller.on('heard_trigger', function() {
+    controller.on("heard_trigger", function() {
         stats.triggers++;
     });
 
-    controller.on('conversationStarted', function() {
+    controller.on("conversationStarted", function() {
         stats.convos++;
     });
 
+    controller.hears(
+        ["^uptime", "^debug"],
+        "direct_message,direct_mention",
+        function(bot, message) {
+            bot.createConversation(message, function(err, convo) {
+                if (!err) {
+                    convo.setVar("uptime", formatUptime(process.uptime()));
+                    convo.setVar("convos", stats.convos);
+                    convo.setVar("triggers", stats.triggers);
 
-    controller.hears(['^uptime','^debug'], 'direct_message,direct_mention', function(bot, message) {
-
-        bot.createConversation(message, function(err, convo) {
-            if (!err) {
-                convo.setVar('uptime', formatUptime(process.uptime()));
-                convo.setVar('convos', stats.convos);
-                convo.setVar('triggers', stats.triggers);
-
-                convo.say('My main process has been online for {{vars.uptime}}. Since booting, I have heard {{vars.triggers}} triggers, and conducted {{vars.convos}} conversations.');
-                convo.activate();
-            }
-        });
-
-    });
-
-    controller.hears(['^say (.*)','^say'], 'direct_message,direct_mention', function(bot, message) {
-        if (message.match[1]) {
-
-            if (!wordfilter.blacklisted(message.match[1])) {
-                bot.reply(message, message.match[1]);
-            } else {
-                bot.reply(message, '_sigh_');
-            }
-        } else {
-            bot.reply(message, 'I will repeat whatever you say.')
+                    convo.say(
+                        "My main process has been online for {{vars.uptime}}. Since booting, I have heard {{vars.triggers}} triggers, and conducted {{vars.convos}} conversations."
+                    );
+                    convo.activate();
+                }
+            });
         }
-    });
+    );
 
+    controller.hears(
+        ["^say (.*)", "^say"],
+        "direct_message,direct_mention",
+        function(bot, message) {
+            if (message.match[1]) {
+                if (!wordfilter.blacklisted(message.match[1])) {
+                    bot.reply(message, message.match[1]);
+                } else {
+                    bot.reply(message, "_sigh_");
+                }
+            } else {
+                bot.reply(message, "I will repeat whatever you say.");
+            }
+        }
+    );
 
     /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
     /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
@@ -65,21 +69,20 @@ module.exports = function(controller) {
     /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
     /* Utility function to format uptime */
     function formatUptime(uptime) {
-        var unit = 'second';
+        var unit = "second";
         if (uptime > 60) {
             uptime = uptime / 60;
-            unit = 'minute';
+            unit = "minute";
         }
         if (uptime > 60) {
             uptime = uptime / 60;
-            unit = 'hour';
+            unit = "hour";
         }
         if (uptime != 1) {
-            unit = unit + 's';
+            unit = unit + "s";
         }
 
-        uptime = parseInt(uptime) + ' ' + unit;
+        uptime = parseInt(uptime) + " " + unit;
         return uptime;
     }
-
 };
