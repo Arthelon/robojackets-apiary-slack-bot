de3aa12d0aa874fae06b9fa45f65491e31806fe9
Daniel Hsing <hsing.daniel@gmail.com>
2018-11-29 16:05:05 -0500

    chore: refactor slash command handlers
    Signed-off-by: Daniel Hsing <hsing.daniel@gmail.com>

--- skills/help.js
+++ skills/help.js
@@ -1,6 +1,21 @@
+// TODO: have help message list all existing queries
+const helpMessage = `This is a help message`;
+const commandHandlers = {
+    "/apiary-help": (bot, message) => {
+        bot.replyPrivate(message, helpMessage);
+    }
+};
+
 module.exports = function(controller) {
     controller.on("slash_command", function(bot, message) {
-        console.log(message);
-        bot.replyPrivate(message, "This is a help message");
+        const command = message.command;
+        if (command in command_handlers) {
+            commandHandlers[command](bot, message);
+        } else {
+            bot.replyPrivate(
+                message,
+                "Sorry, we think you might have entered an invalid slash command. Please try again!"
+            );
+        }
     });
 };
