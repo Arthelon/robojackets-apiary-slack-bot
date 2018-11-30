f037e5867ba6e3c4161e95c3b765d79ef234ce5a
Daniel Hsing <hsing.daniel@gmail.com>
2018-11-30 09:49:06 -0500

    feat: add user shirt size bot listener
    Signed-off-by: Daniel Hsing <hsing.daniel@gmail.com>

--- skills/user.js
+++ skills/user.js
@@ -1,5 +1,5 @@
 const axios = require("axios");
-const debug = require("debug")("skills::dues");
+const debug = require("debug")("skills::user");
 const triggerEvents = "direct_message,direct_mention";
 const httpClient = axios.create({
     baseURL: process.env.API_URL,
@@ -15,8 +15,7 @@ function sendFail(bot, message) {
     );
 }
 
-async function paidDuesListener(bot, message) {
-    const name = message.match[1];
+async function findUser(name) {
     let resp;
     try {
         resp = await httpClient.get("/users/search", {
@@ -28,7 +27,6 @@ async function paidDuesListener(bot, message) {
         debug(err);
         sendFail(bot, message);
     }
-    debug(resp.data.users);
     const users = resp.data.users;
     if (users.length === 0) {
         bot.reply(
@@ -37,7 +35,12 @@ async function paidDuesListener(bot, message) {
         );
         return;
     }
-    const user = users[0];
+    return users[0];
+}
+
+async function paidDuesListener(bot, message) {
+    const name = message.match[1];
+    const user = await findUser(name);
     debug(user);
     if (user.is_active) {
         bot.reply(
@@ -52,10 +55,33 @@ async function paidDuesListener(bot, message) {
     }
 }
 
+async function shirtSizeListener(bot, message) {
+    const name = message.match[1];
+    const user = await findUser(name);
+    debug(user);
+    const shirtSize = user.shirt_size;
+    if (shirtSize) {
+        bot.reply(
+            message,
+            `${user.full_name} has a shirt size of ${shirtSize.toUpperCase()}.`
+        );
+    } else {
+        bot.reply(
+            message,
+            `Sorry, I was not able to find a shirt size for ${user.full_name}.`
+        );
+    }
+}
+
 module.exports = function(controller) {
     controller.hears(
         [/^did (.+) pay dues this semester/i],
         triggerEvents,
         paidDuesListener
+    );
+    controller.hears(
+        [/^what shirt size is (.+)/i],
+        triggerEvents,
+        shirtSizeListener
     );
 };
