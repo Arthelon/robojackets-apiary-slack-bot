4089868422e226273b96ccfddc35bc54387d6e1d
Daniel Hsing <hsing.daniel@gmail.com>
2018-11-30 09:35:14 -0500

    feat: reply to user indicating if user has paid dues
    Signed-off-by: Daniel Hsing <hsing.daniel@gmail.com>

--- skills/dues.js
+++ skills/dues.js
@@ -21,26 +21,38 @@ module.exports = function(controller) {
         triggerEvents,
         async function(bot, message) {
             const name = message.match[1];
+            let resp;
             try {
-                console.log(process.env.API_URL);
-                const resp = await httpClient.get("/users/search", {
+                resp = await httpClient.get("/users/search", {
                     params: {
                         keyword: name
                     }
                 });
-                debug(resp.data);
-                const users = resp.data.users;
-                if (users.length > 0) {
-                    bot.reply(message, `Found a user: ${users[0].full_name}`);
-                } else {
-                    bot.reply(
-                        message,
-                        "Sorry, I was not able to find the user you specified."
-                    );
-                }
             } catch (err) {
                 debug(err);
                 sendFail(bot, message);
+            }
+            debug(resp.data.users);
+            const users = resp.data.users;
+            if (users.length === 0) {
+                bot.reply(
+                    message,
+                    "Sorry, I was not able to find the user you specified."
+                );
+                return;
+            }
+            const user = users[0];
+            debug(user);
+            if (user.is_active) {
+                bot.reply(
+                    message,
+                    `${user.full_name} has paid dues for this semester.`
+                );
+            } else {
+                bot.reply(
+                    message,
+                    `${user.full_name} has not paid dues for this semester.`
+                );
             }
         }
     );
