ad6a680e8bd4971e80f17210a8c2fe826d657ed5
Daniel Hsing <hsing.daniel@gmail.com>
2018-11-30 09:37:12 -0500

    chore: refactor paid dues listener to named function
    Signed-off-by: Daniel Hsing <hsing.daniel@gmail.com>

--- skills/dues.js
+++ skills/dues.js
@@ -15,45 +15,47 @@ function sendFail(bot, message) {
     );
 }
 
+async function paidDuesListener(bot, message) {
+    const name = message.match[1];
+    let resp;
+    try {
+        resp = await httpClient.get("/users/search", {
+            params: {
+                keyword: name
+            }
+        });
+    } catch (err) {
+        debug(err);
+        sendFail(bot, message);
+    }
+    debug(resp.data.users);
+    const users = resp.data.users;
+    if (users.length === 0) {
+        bot.reply(
+            message,
+            "Sorry, I was not able to find the user you specified."
+        );
+        return;
+    }
+    const user = users[0];
+    debug(user);
+    if (user.is_active) {
+        bot.reply(
+            message,
+            `${user.full_name} has paid dues for this semester.`
+        );
+    } else {
+        bot.reply(
+            message,
+            `${user.full_name} has not paid dues for this semester.`
+        );
+    }
+}
+
 module.exports = function(controller) {
     controller.hears(
         [/^did (.+) pay dues this semester/i],
         triggerEvents,
-        async function(bot, message) {
-            const name = message.match[1];
-            let resp;
-            try {
-                resp = await httpClient.get("/users/search", {
-                    params: {
-                        keyword: name
-                    }
-                });
-            } catch (err) {
-                debug(err);
-                sendFail(bot, message);
-            }
-            debug(resp.data.users);
-            const users = resp.data.users;
-            if (users.length === 0) {
-                bot.reply(
-                    message,
-                    "Sorry, I was not able to find the user you specified."
-                );
-                return;
-            }
-            const user = users[0];
-            debug(user);
-            if (user.is_active) {
-                bot.reply(
-                    message,
-                    `${user.full_name} has paid dues for this semester.`
-                );
-            } else {
-                bot.reply(
-                    message,
-                    `${user.full_name} has not paid dues for this semester.`
-                );
-            }
-        }
+        paidDuesListener
     );
 };
